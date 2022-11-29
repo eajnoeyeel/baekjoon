cc1ba9163959b8d2a45a7aab1099eef5fdd0c405
leeyj0304 <80684358+leeyj0304@users.noreply.github.com>
2022-11-29 11:38:25 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int king, queen, rook, bishop, knight, pawn;
+    scanf("%d", &king);
+    scanf("%d", &queen);
+    scanf("%d", &rook);
+    scanf("%d", &bishop);
+    scanf("%d", &knight);
+    scanf("%d", &pawn);
+    printf("%d %d %d %d %d %d", 1 - king, 1 - queen, 2 - rook, 2 - bishop, 2 - knight, 8 - pawn);
+}
