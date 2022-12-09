f3197e61f4d007f2ea930da480f45a605d903c6f
eajnoeyeel <80684358+eajnoeyeel@users.noreply.github.com>
2022-12-09 19:45:43 +0900

    [Bronze I] Title: 더하기 사이클, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int N, temp, result = 0, count = 0;
+    scanf("%d", &N);
+    temp = N;
+    do {
+        int div = temp / 10;
+        int mod = temp % 10;
+        temp = mod * 10 + (div + mod) % 10;
+        count++;
+    } while (N != temp);
+    printf("%d", count);
+}
