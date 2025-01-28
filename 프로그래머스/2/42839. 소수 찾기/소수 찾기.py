206074395dafc9c360e7f75fb9a3a8226a4a57b3
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-01-28 16:11:35 +0900

    [level 2] Title: 소수 찾기, Time: 8.57 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,5 +1,5 @@
 import math
-from itertools import *
+from itertools import permutations
 
 def is_prime(number):
     if number <= 1:
@@ -14,28 +14,21 @@ def solution(numbers):
     answer = 0
 
     digits = []
-    combs = []
     for number in numbers:
-        digits.append(int(number))
-    
-    for i in range(len(digits)):
-        combs.extend(combinations(digits, i + 1))
-
-    perms = set()
-    for comb in combs:
-        perms.update(permutations(comb))
+        digits.append(number)
+    perms = permutations(digits)
 
     results = set()
     for perm in perms:
         result = 0
         for idx in range(len(perm)):
-            results.add(perm[idx])
-            result += perm[idx] * 10 ** idx
+            results.add(int(perm[idx]))
+            result += int(perm[idx]) * 10 ** idx
         results.add(result)
-    print(results)
     
     for result in results:
         if is_prime(result):
             answer += 1
 
     return answer
+
