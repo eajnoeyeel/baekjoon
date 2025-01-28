731c90aac6700eeaab6635d66b8b3a1cdbde1c10
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-01-28 19:34:03 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 19:18:40
+2025년 01월 28일 19:34:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,11 +1,16 @@
 from math import sqrt
 
 def solution(brown, yellow):
+    answer = []
+
     a = 1
-    b = -1 * (2 + 0.5 * brown)
+    b = -(2 + 0.5 * brown)
     c = yellow + brown
 
-    width = int(((-1 * b) + sqrt(b ** 2 - 4 * a * c)) / 2 * a)
+    width = ((-1 * b) + sqrt(abs(b ** 2 - 4 * a * c))) // 2 * a
     height = (brown + yellow) // width
 
-    return [width, height]
+    answer.append(width)
+    answer.append(height)
+    
+    return answer
