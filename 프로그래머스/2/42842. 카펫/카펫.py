58bcda15617010896f30aba22f175fc83bd7c860
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-01-28 19:35:22 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,16 +1,11 @@
 from math import sqrt
 
 def solution(brown, yellow):
-    answer = []
-
     a = 1
     b = -(2 + 0.5 * brown)
     c = yellow + brown
 
     width = ((-1 * b) + sqrt(abs(b ** 2 - 4 * a * c))) // 2 * a
     height = (brown + yellow) // width
-
-    answer.append(width)
-    answer.append(height)
     
-    return answer
+    return [width, height]
