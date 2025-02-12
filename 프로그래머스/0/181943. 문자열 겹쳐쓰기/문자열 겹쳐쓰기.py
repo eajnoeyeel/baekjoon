061f036fa7569229efe85a48325844f901b73dd6
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-02-12 14:32:19 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,10 +1,10 @@
 def solution(my_string, overwrite_string, s):
     answer = ''
     
-    for idx in range(len(my_string)):
-        if s <= idx and idx <= (len(overwrite_string) + s - 1):
-            answer += overwrite_string[idx - s]
-        else:
-            answer += my_string[idx]
-            
-    return answer
+    # for idx in range(len(my_string)):
+    #     if s <= idx and idx <= (len(overwrite_string) + s - 1):
+    #         answer += overwrite_string[idx - s]
+    #     else:
+    #         answer += my_string[idx]
+    
+    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
