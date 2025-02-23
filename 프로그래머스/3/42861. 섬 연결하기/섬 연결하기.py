e4eed4a1db5ec7dbddc93ff0fcdd6e2671493b85
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-02-23 23:51:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,15 +1,21 @@
 def solution(n, costs):
     answer = 0
+
+    # 비용 순 정렬
     costs.sort(key=lambda x : x[2])
+
+    # 시작점 포함
     linked = {costs[0][0]}
     
     while len(linked) != n:
-        for bridge in costs:
-            if bridge[0] in linked and bridge[1] in linked:
+        for start, end, cost in costs:
+            # 이미 연결된 다리는 건너뜀
+            if start in linked and end in linked:
                 continue
-            if bridge[0] in linked or bridge[1] in linked:
-                linked.update([bridge[0], bridge[1]])
-                answer += bridge[2]
+            # 적어도 하나만 연결된 경우 확장
+            if start in linked or end in linked:
+                linked.update([start, end])
+                answer += cost
                 break
 
     return answer
