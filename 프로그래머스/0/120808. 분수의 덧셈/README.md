7d59a6fbd4809d21adc93e5d382929ca9a3aaf53
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2025-04-10 11:38:47 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808#) 
 
 ### 성능 요약
 
-메모리: 9.19 MB, 시간: 0.01 ms
+메모리: 9.26 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 10일 11:37:54
+2025년 04월 10일 11:38:43
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -2,8 +2,8 @@ import math
 
 def solution(numer1, denom1, numer2, denom2):
     answer = []
-    numer3 = int(numer1 * denom2 / math.gcd(denom1, denom2) + numer2 * denom1 / math.gcd(denom1, denom2))
-    denom3 = int(denom1 * denom2 / math.gcd(denom1, denom2))
+    numer3 = int(numer1 * denom2 + numer2 * denom1)
+    denom3 = int(denom1 * denom2)
     
     answer.append(numer3 / math.gcd(numer3, denom3))
     answer.append(denom3 / math.gcd(numer3, denom3))
