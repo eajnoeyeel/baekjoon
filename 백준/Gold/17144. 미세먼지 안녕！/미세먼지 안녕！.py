648be2314f29e570bb3569df525022d65bb38bee
이연재 <80684358+eajnoeyeel@users.noreply.github.com>
2026-02-01 12:49:06 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3740 ms, Memory: 33740 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -2,9 +2,8 @@ from copy import deepcopy
 
 R, C, T = map(int, input().split())
 board = []
-purifiers = []
 dusts = []
-
+purifiers = []
 
 for r in range(R):
     row = list(map(int, input().split()))
@@ -35,7 +34,6 @@ def spread(row, col, temp):
             temp[nr][nc] += amount
             count += 1
     
-    # 현재 위치에서 퍼진 양만큼 감소
     temp[row][col] -= amount * count
 
 
@@ -48,38 +46,31 @@ def purify():
     top, bottom = purifiers[0][0], purifiers[1][0]
 
     # 위쪽 순환 (반시계 방향)
-    # 1. 아래로 (공기청정기 위쪽 열 아래로 밀기)
-    for r in range(top - 1, 0, -1):
+    # 1. 아래로 (공기청정기 위쪽 열 아래로 내리기)
+    for r in range(1, top):
         temp[r][0] = board[r - 1][0]
-    
     # 2. 왼쪽으로 (맨 윗줄 왼쪽 이동)
     for c in range(C - 1):
         temp[0][c] = board[0][c + 1]
-    
     # 3. 위로
     for r in range(top):
         temp[r][C - 1] = board[r + 1][C - 1]
-
     # 4. 오른쪽으로
     for c in range(2, C):
         temp[top][c] = board[top][c - 1]
-
+    
     temp[top][1] = 0
 
     # 아래쪽 순환
-
-    # 1. 위로 (공기청정기 위쪽 열 위로 밀기)
-    for r in range(R - 1, bottom + 1, -1):
-        temp[r - 1][0] = board[r][0]
-    
+    # 1. 위로 (공기청정기 아래쪽 열 위로 밀기)
+    for r in range(bottom + 1, R - 1):
+        temp[r][0] = board[r + 1][0]
     # 2. 왼쪽으로
     for c in range(C - 1):
         temp[R - 1][c] = board[R - 1][c + 1]
-    
     # 3. 아래로
     for r in range(bottom + 1, R):
         temp[r][C - 1] = board[r - 1][C - 1]
-    
     # 4. 오른쪽으로
     for c in range(2, C):
         temp[bottom][c] = board[bottom][c - 1]
@@ -93,7 +84,7 @@ def purify():
     board = temp
 
 
-def solution():
+def main():
     global board, dusts
 
     for _ in range(T):
@@ -103,31 +94,30 @@ def solution():
         # 미세먼지 확산
         for r, c in dusts:
             spread(r, c, temp)
-
+        
         # 확산 결과로 원래 맵 갱신
         board = temp
 
         # 공기청정기 작동
         purify()
-        
+
         # dusts 갱신
         new_dusts = []
         for r in range(R):
             for c in range(C):
                 if board[r][c] > 0:
                     new_dusts.append((r, c))
-
+        
         dusts = new_dusts
     
     total = 0
-
     for r in range(R):
         for c in range(C):
             if board[r][c] > 0:
                 total += board[r][c]
     
-    return total
+    print(total)
 
 
 if __name__ == "__main__":
-    print(solution())
+    main()
